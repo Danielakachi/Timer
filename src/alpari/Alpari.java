8749f51fa740947fd0e138840190c07ded741cd9
Danielakachi <akachidanil@gmail.com>
2018-02-01 06:29:31 +0100

    design

---
+++ src/alpari/Alpari.java
@@ -0,0 +1,21 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package alpari;
+
+/**
+ *
+ * @author Dell E6540
+ */
+public class Alpari {
+   
+    public static void main(String[] args) {
+       alp a=new alp();
+       a.setVisible(true);
+       a.setSize(900,800);
+       
+    }
+    
+}
